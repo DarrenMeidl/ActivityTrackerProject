89f10814b6bec6173f460b699c7ee2f46464b077
DarrenMeidl <dmeidl15@gmail.com>
2023-11-05 14:32:50 +0000

    Created Container class

--- src/Activities.java
+++ src/Activities.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
 public class Activities {
-
+    ArrayList<Activity> activityList;
+    public Activities(){ this.activityList = new ArrayList<>(); }
 }
