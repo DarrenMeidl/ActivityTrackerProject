ee58244202ad6d7823ffb8134fd0e26211e45cf0
DarrenMeidl <dmeidl15@gmail.com>
2023-11-05 14:39:53 +0000

    Added add object to array list method

--- src/Activities.java
+++ src/Activities.java
@@ -1,13 +1,17 @@
 import java.util.ArrayList;
 import java.util.Collections;
+//Activities Container Class
 public class Activities {
     ArrayList<Activity> activityList; //ArrayList field called activityList - Darren
     public Activities(){ this.activityList = new ArrayList<>(); } //Sets this activityList field to a new ArrayList
 
 
 
-
-    //Sort method
+    //Adds object to activityList method - Darren
+    public void add(Activity act){
+        activityList.add(act);
+    }
+    //Sort method - Darren
     public void sort(){
         Collections.sort(activityList); //Sorts by natural ordering - Darren
     }
