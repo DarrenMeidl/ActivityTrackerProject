8f4da36507d7c550efdd1a36885d0edc1ea013aa
DarrenMeidl <dmeidl15@gmail.com>
2023-11-05 14:36:55 +0000

    Added ArrayList & coded natural ordering sort method

--- src/Activities.java
+++ src/Activities.java
@@ -1,6 +1,14 @@
 import java.util.ArrayList;
 import java.util.Collections;
 public class Activities {
-    ArrayList<Activity> activityList;
-    public Activities(){ this.activityList = new ArrayList<>(); }
+    ArrayList<Activity> activityList; //ArrayList field called activityList - Darren
+    public Activities(){ this.activityList = new ArrayList<>(); } //Sets this activityList field to a new ArrayList
+
+
+
+
+    //Sort method
+    public void sort(){
+        Collections.sort(activityList); //Sorts by natural ordering - Darren
+    }
 }
